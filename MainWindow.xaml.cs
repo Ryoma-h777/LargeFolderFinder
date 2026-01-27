8dcfaa57575029a23e246fd56f40888b7b37e9a7
平安山良真 <chocolate.rh23@gmail.com>
2026-01-27 12:48:25 +0900

    GetCharWidth_MSGothicの条件を調整

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1109,9 +1109,28 @@ namespace LargeFolderFinder
             return width;
         }
 
-        private int GetCharWidth(char c)
+        private int GetCharWidth_MSGothic(char c)
         {
-            if (c < 0x81 || (0xff61 <= c && c < 0xffa0)) return 1;
+            if (c < 0x81) return 1; // ASCII
+            if (0xff61 <= c && c < 0xffa0) return 1; // 半角カナ
+
+            switch (c)
+            {
+                case (char)0x00B7: // ·
+                case (char)0x2011: // - non-breaking hyphen
+                case (char)0x2017: // ‗ DOUBLE LOW LINE
+                    return 1;
+                    //case (char)0x2010: // ‐ Hyphen
+                    //case (char)0x00B1: // ±
+                    //case (char)0x00D7: // ×
+                    //case (char)0x00F7: // ÷
+                    //case (char)0x2018: // ‘ LEFT SINGLE QUOTATION MARK
+                    //case (char)0x2019: // ’ RIGHT SINGLE QUOTATION MARK
+                    //case (char)0x201C: // “ LEFT DOUBLE QUOTATION MARK
+                    //case (char)0x201D: // ” RIGHT DOUBLE QUOTATION MARK
+                    //case (char)0x2026: // …
+                    //return 2;
+            }
             var category = Char.GetUnicodeCategory(c);
             if (category == UnicodeCategory.NonSpacingMark
                 || category == UnicodeCategory.EnclosingMark
